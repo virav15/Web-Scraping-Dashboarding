28e342b0906be44d1916bfa4ad1e88d571a41996
virav15 <47110597+virav15@users.noreply.github.com>
2019-04-26 14:35:31 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+#import dependencies
+from bs4 import BeautifulSoup as bs
+from splinter import Browser
+import os
+import pandas as pd
+import time
+from selenium import webdriver
+import import_ipynb
+import mission_to_mars
