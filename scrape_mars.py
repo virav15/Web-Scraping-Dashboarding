e840df2654d22d6e710d35343a47f0b05b86b57e
virav15 <47110597+virav15@users.noreply.github.com>
2019-04-26 14:24:26 -0400

    Update scrape_mars.py

--- scrape_mars.py
+++ scrape_mars.py
@@ -43,3 +43,43 @@ def scrape():
     finally:
 
         browser.quit()
+
+    # FEATURED IMAGE
+def scrape_mars_image():
+
+    try: 
+
+        # Initialize browser 
+        browser = init_browser()
+
+        #browser.is_element_present_by_css("img.jpg", wait_time=1)
+
+        # Visit Mars Space Images through splinter module
+        image_url_featured = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+        browser.visit(image_url_featured)# Visit Mars Space Images through splinter module
+
+        # HTML Object 
+        html_image = browser.html
+
+        # Parse HTML with Beautiful Soup
+        soup = bs(html_image, 'html.parser')
+
+        # Retrieve background-image url from style tag
+        featured_image_url  = soup.find('article')['style'].replace('background-image: url(','').replace(');', '')[1:-1]
+
+        # Website Url
+        main_url = 'https://www.jpl.nasa.gov'
+
+        # Concatenate website url with scrapped route
+        featured_image_url = main_url + featured_image_url
+
+        # Display full link to featured image
+        featured_image_url 
+
+        # Dictionary entry from FEATURED IMAGE
+        mars_info['featured_image_url'] = featured_image_url 
+        
+        return mars_info
+    finally:
+
+        browser.quit()
